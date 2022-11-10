db1a52d2685538f4718d02bdfe29feffe65ed127
singh12avi <44125320+singh12avi@users.noreply.github.com>
2022-11-10 23:36:48 +0530

    Update Streamlit_app.py
    Added new header

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -6,7 +6,9 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmral')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-treamlit.text('🥑🍞Avocado Toast')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
